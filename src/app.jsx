6b4b8025aa019ea2cf9c7e241d531bf6942509ac
Funncy <gywns41@naver.com>
2021-02-13 22:18:29 +0900

    Add login components

--- src/app.jsx
+++ src/app.jsx
@@ -1,8 +1,15 @@
 // import logo from './logo.svg';
-import './app.css';
+import styles from './app.module.css';
+import Login from './components/login/login';
 
-function App() {
-	return <h1> Hello :)</h1>;
+function App({ authService }) {
+	return (
+		<>
+			<div className={styles.app}>
+				<Login authService={authService} />
+			</div>
+		</>
+	);
 }
 
 export default App;
--- src/service/firebase.js
+++ src/service/firebase.js
@@ -1,8 +1,10 @@
+import firebase from 'firebase';
+
 var firebaseConfig = {
 	apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
 	authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
 	projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
 };
 // Initialize Firebase
-firebase.initializeApp(firebaseConfig);
-firebase.analytics();
+const firebaseApp = firebase.initializeApp(firebaseConfig);
+export default firebaseApp;
